a5e112018fdb98030c2296d1e79dbf8bccbe9ff9
CharlesChiang <charleschiang2343@gmail.com>
2021-04-16 09:49:33 +0800

    V2

--- WAFlog-ETL-Generator/WAFlog-ETL-Generator.py
+++ WAFlog-ETL-Generator/WAFlog-ETL-Generator.py
@@ -12,10 +12,11 @@ import datetime
 time = datetime.datetime.now()
 
 s3 = boto3.client('s3')
+
 FILE_TO_READ = 'aws-waf-logs'
 
 def read_file_gen():
-    with open('/tmp/test.json') as f:
+    with open('/tmp/test.txt') as f:
         for line in f:
             yield json.loads(line)
 
@@ -27,44 +28,32 @@ def lambda_handler(event, context):
     while True:
         try:
             waflog = next(it)
-            waflog['requestId'] = str(waflog['httpRequest']['requestId']).replace(" ", "")
-            waflog['action'] = str(waflog['action']).replace(" ", "")
-            waflog['terminatingRuleType'] = str(waflog['terminatingRuleType']).replace(" ", "")
-            waflog['webaclId'] = str(waflog['webaclId']).replace(" ", "")
-
-            if len(str(waflog['timestamp'])) <= 0:
-                waflog['timestamp'] = 'NA'
-            else:
-                waflog['timestamp'] = datetime.datetime.fromtimestamp(float((str(waflog['timestamp']).replace(" ", ""))[0:10])).isoformat()
-
-            if len(waflog['webaclId']) <= 0:
-                waflog['webaclId'] = 'NA'
-            else:
-                pass
-
-            if len(waflog['terminatingRuleId']) <= 0:
-                    terminatingRuleId = 'NA'
-            else:
-                pass
-
-            if len(waflog['terminatingRuleType']) <= 0:
-                waflog['terminatingRuleType'] = 'NA'
-            else:
-                pass
-
-            if len(waflog['action']) <= 0:
-                waflog['action'] = 'NA'
-            else:
-                pass
-
-            if len(waflog['requestId']) <= 0:
-                waflog['requestId'] = 'NA'
-
-            waflog = dict((k, waflog[k]) for k in ['timestamp', 'webaclId', 'terminatingRuleType', 'action', 'requestId']
-                                                    if k in waflog)
-            with open(f'/tmp/after.json', 'a', encoding='utf-8') as g:
-                g.write(json.dumps(waflog))
-                g.write('\n')
+            
+            convert_timestamp = lambda x: datetime.datetime.fromtimestamp(float((str(x).replace(" ", ""))[0:10])).isoformat() if len(str(x)) > 0 else 'NA'
+            
+            waflog['timestamp']  = convert_timestamp(waflog['timestamp'])
+            
+            replaceblank = lambda x : str(x).replace(" ", "")
+            log_length = lambda x : x if len(str(x)) > 0 else 'NA'
+            
+            waflog['requestId'] = replaceblank(waflog['httpRequest']['requestId'])
+            waflog['requestId'] = log_length(waflog['requestId'])
+            
+            waflog['action'] = replaceblank(waflog['action'])
+            waflog['action'] = log_length(waflog['action'])
+            
+            waflog['terminatingRuleType'] = replaceblank(waflog['terminatingRuleType'])
+            waflog['terminatingRuleType'] = log_length(waflog['terminatingRuleType'])
+            
+            waflog['webaclId'] = replaceblank(waflog['webaclId'])
+            waflog['webaclId'] = log_length(waflog['webaclId'])
+            
+            waflog = dict((k, waflog[k]) for k in ['timestamp', 'webaclId', 'terminatingRuleType', 'action', 'requestId'] if k in waflog)
+                                                    
+            with open('/tmp/after.json', 'a', encoding='utf-8') as file:
+                file.write(json.dumps(waflog))
+                file.write('\n')
+                
         except StopIteration:
             break
 
